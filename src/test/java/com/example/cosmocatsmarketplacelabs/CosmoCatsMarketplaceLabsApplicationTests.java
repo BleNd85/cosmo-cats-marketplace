5112d1423e2a036524b8a172bb53370fd4fefce5
VladPC <volodkatraktorist@gmail.com>
2024-12-08 16:45:18 +0200

    init commit

---
+++ src/test/java/com/example/cosmocatsmarketplacelabs/CosmoCatsMarketplaceLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cosmocatsmarketplacelabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmoCatsMarketplaceLabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
