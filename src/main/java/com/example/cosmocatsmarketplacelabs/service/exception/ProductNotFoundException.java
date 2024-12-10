994a989c34f929ce5b5fa8334b935413092a9854
VladPC <volodkatraktorist@gmail.com>
2024-12-10 18:27:39 +0200

    adds documentation

--- src/main/java/com/example/cosmocatsmarketplacelabs/service/exception/ProductNotFoundException.java
+++ src/main/java/com/example/cosmocatsmarketplacelabs/service/exception/ProductNotFoundException.java
@@ -1,7 +1,7 @@
 package com.example.cosmocatsmarketplacelabs.service.exception;
 
 public class ProductNotFoundException extends RuntimeException {
-    private static final String PRODUCT_NOT_FOUND_MESSAGE = "Customer with id %s not found";
+    private static final String PRODUCT_NOT_FOUND_MESSAGE = "Product with id %s not found";
 
     public ProductNotFoundException(Long id) {
         super(String.format(PRODUCT_NOT_FOUND_MESSAGE, id));
